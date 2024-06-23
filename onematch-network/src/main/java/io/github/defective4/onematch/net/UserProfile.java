4c3dbc7f0d8a1428ba04ae50f1348d44ec314fb1
Defective <def3ctive4@gmail.com>
2024-06-23 21:29:23 +0200

    Add all args constructor

--- onematch-network/src/main/java/io/github/defective4/onematch/net/UserProfile.java
+++ onematch-network/src/main/java/io/github/defective4/onematch/net/UserProfile.java
@@ -6,5 +6,17 @@ public class UserProfile {
     public long joinedDate;
     public String name = "Username";
 
+    public UserProfile(int bestStreak, int currentStreak, int dailyPlace, int allTimePlace, int solvedChallenges,
+            String bestTime, long joinedDate, String name) {
+        this.bestStreak = bestStreak;
+        this.currentStreak = currentStreak;
+        this.dailyPlace = dailyPlace;
+        this.allTimePlace = allTimePlace;
+        this.solvedChallenges = solvedChallenges;
+        this.bestTime = bestTime;
+        this.joinedDate = joinedDate;
+        this.name = name;
+    }
+
     public UserProfile() {}
 }
